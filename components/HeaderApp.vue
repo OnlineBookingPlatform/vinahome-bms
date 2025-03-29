7637ffdb4a36a0ae3137bf88051c28a6d2af9c73
Đặng Tuấn Thành <tuanthanhktltk21@gmail.com>
2025-03-29 17:27:52 +0700

    init icon home

--- components/HeaderApp.vue
+++ components/HeaderApp.vue
@@ -80,6 +80,24 @@ const menuItems = [
   <header class="bg-[#0072bc] p-2">
     <nav class="flex items-center justify-between">
       <ul class="flex space-x-6">
+        <li class="flex items-center ml-2">
+          <NuxtLink to="/dashboard">
+            <span class="text-white text-base flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+                width="17"
+                height="17"
+                fill="currentColor"
+                class="mb-[2px]"
+              >
+                <path
+                  d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"
+                />
+              </svg>
+            </span>
+          </NuxtLink>
+        </li>
         <li v-for="(menu, index) in menuItems" :key="index">
           <el-dropdown>
             <span
@@ -117,11 +135,15 @@ const menuItems = [
       </ul>
       <ul class="flex space-x-6">
         <li>
-          <span class="text-white text-base flex items-center mr-5">Đặng Tuấn Thành</span>
+          <span class="text-white text-base flex items-center mr-5"
+            >Đặng Tuấn Thành</span
+          >
         </li>
         <li>
           <el-dropdown trigger="click">
-            <span class="el-dropdown-link text-white text-base flex items-center p-[1px]">
+            <span
+              class="el-dropdown-link text-white text-base flex items-center p-[1px]"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
@@ -135,7 +157,7 @@ const menuItems = [
                 />
               </svg>
             </span>
-            <template #dropdown> 
+            <template #dropdown>
               <div class="px-2 py-1">
                 <el-input style="width: 500px" placeholder="Tìm kiếm vé" />
               </div>
